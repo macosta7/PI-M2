2cdc07b5d641e35ac3bc93c437cf863c0eb1b055
macosta7 <marcela.costa@sou.inteli.edu.br>
2025-05-05 16:02:55 -0300

    docs: cria todas as pastas que vao ser utilizadas

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+    testEnvironment: 'node',
+    collectCoverage: true,
+    collectCoverageFrom: [
+      'controllers/**/*.js',
+      'models/**/*.js',
+      'routes/**/*.js',
+      '!**/node_modules/**',
+    ],
+    coverageDirectory: 'coverage',
+  };
+  
